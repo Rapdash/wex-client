dcfc1f96bb7adae46977ebaec00d2fa9fec8c4f6
Frederick Johnson <fritzj96@gmail.com>
2020-05-21 19:21:05 -0700

    added: font support

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ['Comfortaa', 'sans-serif'],
+      body: ['Bitter', 'serif']
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
