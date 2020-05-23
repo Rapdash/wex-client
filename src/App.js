36566709a56b2b2ddf33385c4e89f23640f7d403
Frederick Johnson <fritzj96@gmail.com>
2020-05-22 20:21:10 -0700

    added: redirect to /listings from /

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Header } from "./components/Header";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Redirect } from "react-router-dom";
 
 export const App = () => (
   <BrowserRouter>
+    <Redirect from="/" to="/listings" exact />
     <Header />
     <div />
   </BrowserRouter>
