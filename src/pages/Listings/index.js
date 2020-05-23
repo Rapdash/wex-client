fcf4774bbbc30978fd104778d289957d5b3c59df
Frederick Johnson <fritzj96@gmail.com>
2020-05-23 16:59:45 -0700

    added: stubbed firebase hook to Listings page

--- src/pages/Listings/index.js
+++ src/pages/Listings/index.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { firestore } from "firebase/app";
+import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
 
 export const ListingsPage = () => {
+  const [listings, loading, error] = useCollectionDataOnce(firestore().collection('listings'));
+  console.log(listings);
   return <div />;
 };
