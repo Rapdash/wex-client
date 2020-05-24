7ac6edcf573a39faf588d07bfc6fd58595528c8d
Frederick Johnson <fritzj96@gmail.com>
2020-05-23 17:06:15 -0700

    progress on listings page

--- src/pages/Listings/index.js
+++ src/pages/Listings/index.js
@@ -3,7 +3,8 @@ import { firestore } from "firebase/app";
 import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
 
 export const ListingsPage = () => {
-  const [listings, loading, error] = useCollectionDataOnce(firestore().collection('listings'));
-  console.log(listings);
-  return <div />;
+  const [listings, loading, error] = useCollectionDataOnce(
+    firestore().collection("listings")
+  );
+  return <main></main>;
 };
