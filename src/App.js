49ba68b18bab8e91d01c122f65ef73d338289fa9
Frederick Johnson <fritzj96@gmail.com>
2020-05-23 17:18:04 -0700

    added: auth check in app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { auth } from "firebase/app";
 import { BrowserRouter, Redirect, Route } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 import { Header } from "./shared/Header";
 
 import { ListingsPage } from "./pages/Listings";
 import { LoginPage } from "./pages/Login";
 
-export const App = () => (
-  <BrowserRouter>
-    <Redirect from="/" to="/listings" exact />
-    <Header />
-    <Route path="/login" component={LoginPage} />
-    <Route path="/listings" component={ListingsPage} />
-  </BrowserRouter>
-);
+export const App = () => {
+  const [user, loading] = useAuthState(auth());
+  return (
+    <BrowserRouter>
+      {!user && !loading && <Redirect to="/login" />}
+      {user && <Redirect from="/" to="/listings" exact />}
+      <Header />
+      <Route path="/login" component={LoginPage} />
+      <Route path="/listings" component={ListingsPage} />
+    </BrowserRouter>
+  );
+};
