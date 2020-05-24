258585a08c220eaac460624b4944a9dc5b14bf90
Frederick Johnson <fritzj96@gmail.com>
2020-05-23 17:15:03 -0700

    added: LoginPage to app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter, Redirect, Route } from "react-router-dom";
 
 import { Header } from "./shared/Header";
+
 import { ListingsPage } from "./pages/Listings";
+import { LoginPage } from "./pages/Login";
 
 export const App = () => (
   <BrowserRouter>
     <Redirect from="/" to="/listings" exact />
     <Header />
+    <Route path="/login" component={LoginPage} />
     <Route path="/listings" component={ListingsPage} />
   </BrowserRouter>
 );
